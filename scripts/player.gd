1ddcc09a27f2bc0c83ff9821aada4f1c2d5a1fb5
Lelek <106480210+LelekDEV@users.noreply.github.com>
2024-11-22 22:24:04 +0100

    Add player and tileset textures

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var sprite: Node = $AnimatedSprite2D
+
 var speed: float = 400
 var jump_height: float = -900
 var gravity: float = 2000
@@ -21,3 +23,11 @@ func _physics_process(delta: float):
 	
 	if Input.is_action_just_pressed("time_stop_toggle"):
 		Global.time_stop = not Global.time_stop
+		
+	if not is_on_floor():
+		sprite.play("jump")
+	elif direction == 0:
+		sprite.play("idle")
+	else:
+		sprite.play("walk")
+		sprite.flip_h = direction < 0
