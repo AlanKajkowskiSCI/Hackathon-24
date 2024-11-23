8250806e5a06d352b6a6326c46cebb1b48183aea
AlanKajkowskiSCI <alankajkowski@microsoft.sci.edu.pl>
2024-11-23 14:24:37 +0100

    .

--- scripts/power_collectible.gd
+++ scripts/power_collectible.gd
@@ -3,5 +3,6 @@ extends Area2D
 @onready var animation: Node = get_parent().get_node("CanvasLayer/AnimationPlayer")
 
 func _on_body_entered(_body: Node2D):
-	animation.play("new_power")
-	queue_free()
+	#animation.play("new_power")
+	#queue_free()
+	pass
