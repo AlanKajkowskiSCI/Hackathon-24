8280fa36bdc85de344f664b04248e5d8f2b8ab72
Lelek <106480210+LelekDEV@users.noreply.github.com>
2024-11-23 08:33:02 +0100

    Further content

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,6 +7,8 @@ func _ready():
 	$MovingObstacles/MovingObstacle2/Area2D/AnimationPlayer.seek(0.3)
 	$MovingObstacles/MovingObstacle6/Area2D/AnimationPlayer.seek(0.3)
 	$MovingObstacles/MovingObstacle7/Area2D/AnimationPlayer.seek(0.3)
+	$MovingObstacles/MovingObstacle11/Area2D/AnimationPlayer.seek(0.1)
+	$MovingObstacles/MovingObstacle12/Area2D/AnimationPlayer.seek(0.2)
 	
 func _process(_delta: float):
 	color_rect.visible = Global.time_stop
